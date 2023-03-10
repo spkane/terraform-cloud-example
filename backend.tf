b210217bf7e614be1d47b88314f148be1092c4f4
Sean P. Kane <spkane00@gmail.com>
2023-03-10 13:58:56 -0800

    Replace backend remote block with cloud block

--- backend.tf
+++ backend.tf
@@ -2,10 +2,10 @@
 # Additional Copyright (c) Sean P. Kane
 # SPDX-License-Identifier: MPL-2.0
 
-# The block below configures Terraform to use the 'remote' backend with Terraform Cloud.
+# The block below configures Terraform to use Terraform Cloud.
 # For more information, see https://www.terraform.io/docs/backends/types/remote.html
 terraform {
-  backend "remote" {
+  cloud {
     organization = "{{ORGANIZATION_NAME}}"
 
     workspaces {
